dac2d3dee6e9b7fb7f7659a94622636f6c082949
landonrepp <landonrepp@gmail.com>
2019-03-27 23:47:43 -0500

    modified:   server/client/navigation.js

--- server/client/navigation.js
+++ server/client/navigation.js
@@ -27,7 +27,7 @@ $(document).ready(()=>{
 	$(".lineItemHeader > .lineItemInfo").click(()=>{
 		var headerIDS = ["#nameHead","#buyHead","#sellHead","#profitHead"];
 		var tempStr = "fqoj"; //$(`${$(this).attr("id")} > .sort`).html();
-		alert($(this).attr("id"));
+		alert($(this).css("background-color","red"));
 		for(var i in headerIDS){
 			$(`${headerIDS[i]} > .sort`).html("&#9658;");
 		}
