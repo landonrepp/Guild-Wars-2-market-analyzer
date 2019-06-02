1eabb4d6d85e8fa6c71eb517985b12e5564d3ca6
landonrepp <landonrepp@gmail.com>
2019-06-02 11:34:46 -0500

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -150,7 +150,7 @@ app.get('/:path',(req,res)=>{
 app.route('/sql/sppost/:sp').post((req,res)=>{
     let sp=req.params['sp'];
     console.log(req.params);
-    console.log(req);
+    console.log(req.headers);
 
     callSp(sp).then(result=>{
         res.end(JSON.stringify(result[0]));
